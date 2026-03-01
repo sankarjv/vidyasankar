aaf8352c30bb76cce5f9da5f902ef388db34614f
sankarjv <sankarjv@users.noreply.github.com>
2026-03-01 16:44:06 +0530

    Refactor service worker fetch event handling
    Signed-off-by: sankarjv <sankarjv@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,102 @@
-self.addEventListener('fetch', event => {
 
-const url = new URL(event.request.url);
+    // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
-if (!HOSTNAME_WHITELIST.includes(url.hostname)) {
-    return;
-}
+    /*
+      Welcome to our basic Service Worker! This Service Worker offers a basic offline experience
+      while also being easily customizeable. You can add in your own code to implement the capabilities
+      listed below, or change anything else you would like.
 
-const cached = caches.match(event.request);
-const fixedUrl = getFixedUrl(event.request);
-const fetched = fetch(fixedUrl, { cache: 'no-store' });
-const fetchedCopy = fetched.then(r => r.clone());
 
-const delayCacheResponse =
-    new Promise(resolve =>
-        setTimeout(resolve, 500, cached)
-    );
+      Need an introduction to Service Workers? Check our docs here: https://docs.pwabuilder.com/#/home/sw-intro
+      Want to learn more about how our Service Worker generation works? Check our docs here: https://docs.pwabuilder.com/#/studio/existing-app?id=add-a-service-worker
 
-event.respondWith(
-    Promise.race([fetched.catch(()=>cached), delayCacheResponse])
-        .then(resp => resp || fetched)
-        .catch(() =>
-            new Response("Offline", { status: 503 })
+      Did you know that Service Workers offer many more capabilities than just offline? 
+        - Background Sync: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/06
+        - Periodic Background Sync: https://web.dev/periodic-background-sync/
+        - Push Notifications: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=push-notifications-on-the-web
+        - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
+    */
+
+    const HOSTNAME_WHITELIST = [
+        self.location.hostname,
+        'fonts.gstatic.com',
+        'fonts.googleapis.com',
+        'cdn.jsdelivr.net'
+    ]
+    const NETWORK_TIMEOUT_MS = 500
+
+    // The Util Function to hack URLs of intercepted requests
+    const getFixedUrl = (req) => {
+        var now = Date.now()
+        var url = new URL(req.url)
+
+        // 1. fixed http URL
+        // Just keep syncing with location.protocol
+        // fetch(httpURL) belongs to active mixed content.
+        // And fetch(httpRequest) is not supported yet.
+        url.protocol = self.location.protocol
+
+        // 2. add query for caching-busting.
+        // Github Pages served with Cache-Control: max-age=600
+        // max-age on mutable content is error-prone, with SW life of bugs can even extend.
+        // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
+        // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
+        if (url.hostname === self.location.hostname) {
+            url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
+        }
+        return url.href
+    }
+
+    /**
+     *  @Lifecycle Activate
+     *  New one activated when old isnt being used.
+     *
+     *  waitUntil(): activating ====> activated
+     */
+    self.addEventListener('activate', event => {
+      event.waitUntil(self.clients.claim())
+    })
+
+    /**
+     *  @Functional Fetch
+     *  All network requests are being intercepted here.
+     *
+     *  void respondWith(Promise<Response> r)
+     */
+    self.addEventListener('fetch', event => {
+    // Skip some of cross-origin requests, like those for Google Analytics.
+    if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+        // Stale-while-revalidate
+        // similar to HTTP's stale-while-revalidate: https://www.mnot.net/blog/2007/12/12/stale
+        // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
+        const cached = caches.match(event.request)
+        const fixedUrl = getFixedUrl(event.request)
+        const fetched = fetch(fixedUrl, { cache: 'no-store' })
+        const fetchedCopy = fetched.then(resp => resp.clone())
+
+        // Call respondWith() with whatever we get first.
+        // If the fetch fails (e.g disconnected), wait for the cache.
+        // If there’s nothing in cache, wait for the fetch.
+        // If neither yields a response, return offline pages.
+
+        // Updated 2/7/2023 by @zteutsch. Switched from pure race between cache and network to a half second (500 ms) timeout
+        // before going to cache. All fallbacks should still work and cache will be updated in the background.
+
+        const delayCacheResponse = new Promise((resolve) => {
+            setTimeout(resolve, 500, cached);
+        })
+
+        event.respondWith(
+        Promise.race([fetched.catch(_ => cached), delayCacheResponse])
+            .then(resp => resp || fetched)
+            .catch(_ => { /* eat any errors */ })
         )
-);
 
-event.waitUntil(
-    Promise.all([fetchedCopy, caches.open("pwa-cache")])
-        .then(([resp, cache]) =>
-            resp.ok && cache.put(event.request, resp)
+        // Update the cache with the version we fetched (only for ok status)
+        event.waitUntil(
+        Promise.all([fetchedCopy, caches.open("pwa-cache")])
+            .then(([response, cache]) => response.ok && cache.put(event.request, response))
+            .catch(_ => { /* eat any errors */ })
         )
-);
-});
+      }
+    })
