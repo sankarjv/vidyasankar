eae4160da26f4dc6e0a638f328ecccdca426c92a
sankarjv <sankarjv@users.noreply.github.com>
2026-03-01 17:08:53 +0530

    Refactor service worker for improved fetch handling
    Signed-off-by: sankarjv <sankarjv@users.noreply.github.com>

---
+++ sw.js
@@ -0,0 +1,33 @@
+self.addEventListener('fetch', event => {
+
+const url = new URL(event.request.url);
+
+if (!HOSTNAME_WHITELIST.includes(url.hostname)) {
+    return;
+}
+
+const cached = caches.match(event.request);
+const fixedUrl = getFixedUrl(event.request);
+const fetched = fetch(fixedUrl, { cache: 'no-store' });
+const fetchedCopy = fetched.then(r => r.clone());
+
+const delayCacheResponse =
+    new Promise(resolve =>
+        setTimeout(resolve, 500, cached)
+    );
+
+event.respondWith(
+    Promise.race([fetched.catch(()=>cached), delayCacheResponse])
+        .then(resp => resp || fetched)
+        .catch(() =>
+            new Response("Offline", { status: 503 })
+        )
+);
+
+event.waitUntil(
+    Promise.all([fetchedCopy, caches.open("pwa-cache")])
+        .then(([resp, cache]) =>
+            resp.ok && cache.put(event.request, resp)
+        )
+);
+});
